e90cef051704f00913854237c0dbadebb3566eb6
Fatih Talha Özdemir <fatihtalhaozdemir@gmail.com>
2024-08-15 13:17:20 +0700

    todo label max width fix

--- resources/js/components/todo-item.vue
+++ resources/js/components/todo-item.vue
@@ -4,7 +4,7 @@
             <input v-model="data.completed" :id="`todo-${data.id}`" :name="`todo-${data.id}`" type="checkbox" @change="handleTodoUpdated(data)"
                 class="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded cursor-pointer">
             <label :for="`todo-${data.id}`" class="items-center ml-3 flex flex-1 justify-between text-gray-900 cursor-pointer" :class="{'line-through opacity-25': data.completed}">
-                <span class="text-lg font-medium">{{ data.title }}</span>
+                <span class="text-lg font-medium overflow-hidden text-ellipsis whitespace-nowrap max-w-[160px]">{{ data.title }}</span>
                 <span class="text-sm font-light text-gray-500">{{ data.created_at_formatted }}</span>
             </label>
             <button class="ml-5" @click="handleTodoRemove(data.id)" >Remove</button>
